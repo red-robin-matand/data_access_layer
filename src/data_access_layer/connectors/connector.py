2d614258f43b55990311173fbf72fe74bc0d9f8d
red-robin-matand <red.robin.matand@gmail.com>
2025-04-22 23:32:01 +0300

    Refactor Connector class to improve configuration handling and simplify data source management

--- src/data_access_layer/connectors/connector.py
+++ src/data_access_layer/connectors/connector.py
@@ -1,40 +1,69 @@
 from abc import ABC, abstractmethod
 from enum import Enum
-from functools import cached_property
 
+from data_access_layer.connectors.exceptions.connector import MissingConfigurationKey
 from data_access_layer.datasources import (
     DataSource,
-    KafkaConsumerDataSource,
     DataSourceManager
 )
 
+
 class Connector(ABC):
 
-    def __init__(self, name: str, kafka_consumer_datasource_name : str, data_sink_name : str) -> None:
-        self._name = name
-        self._kafka_consumer_datasource_name = kafka_consumer_datasource_name
-        self._data_sink_name = data_sink_name
+    class ConfigKeys(Enum):
+        NAME = 'name'
+        SOURCE_NAME = 'source_name'
+        SINK_NAME = 'sink_name'
 
-        self._data_source_manager : DataSourceManager = None
-        self._kafka_consumer_datasource : KafkaConsumerDataSource = None
-        self._data_sink : DataSource = None
+        @classmethod
+        def required_keys(cls):
+            [member.value for member in cls]
+
+    def __init__(self, name: str, source_name: str, sink_name: str) -> None:
+        self._name = name
+        self._source_name = source_name
+        self._sink_name = sink_name
+
+        self._data_source_manager: DataSourceManager = None
+        self._source: DataSource = None
+        self._sink: DataSource = None
 
     @property
     def name(self) -> str:
         return self._name
 
+    @classmethod
+    def validate_dict_keys(cls, conf_dict: dict, required_keys: list) -> None:
+
+        missing_keys = [key for key in required_keys if key not in conf_dict]
+        if missing_keys:
+            raise MissingConfigurationKey(
+                f"Missing required keys in the configuration: {missing_keys}")
+
+    @classmethod
+    def from_dict(cls, conf_dict: dict):
+        config_keys = cls.ConfigKeys.required_keys()
+        cls.validate_dict_keys(conf_dict, config_keys)
+
+        return cls(
+            conf_dict[cls.ConfigKeys.NAME.value],
+            conf_dict[cls.ConfigKeys.SOURCE_NAME.value],
+            conf_dict[cls.ConfigKeys.SINK_NAME.value],
+        )
+
     def connect(self) -> None:
         self._data_source_manager = DataSourceManager()
-        self._kafka_consumer_datasource = self._data_source_manager.get_data_source(self._kafka_consumer_datasource_name)
-        self._data_sink = self._data_source_manager.get_data_source(self._data_sink_name)
+        self._source = self._data_source_manager.get_data_source(
+            self._source_name)
+        self._sink = self._data_source_manager.get_data_source(self._sink_name)
 
     def disconnect(self) -> None:
-        self._data_sink.disconnect()
-        self._kafka_consumer_datasource.disconnect()
+        self._sink.disconnect()
+        self._source.disconnect()
 
     def check_health(self) -> None:
-        self._data_sink.check_health()
-        self._kafka_consumer_datasource.check_health()
+        self._sink.check_health()
+        self._source.check_health()
 
     def __enter__(self) -> None:
         self.connect()
@@ -47,5 +76,5 @@ class Connector(ABC):
         self.disconnect()
 
     @abstractmethod
-    def consume_from_broker_to_sink(self, sink_args : dict) -> None:
+    def source_to_sink(self, source_args: dict, sink_args: dict) -> None:
         pass
