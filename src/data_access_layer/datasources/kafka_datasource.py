633cece5d68740f558feeb44f84fbabdf8fc79eb
red-robin-matand <red.robin.matand@gmail.com>
2025-04-30 19:17:19 +0000

    Fix timeout reference in get_broker_metadata method to use instance variable

--- src/data_access_layer/datasources/kafka_datasource.py
+++ src/data_access_layer/datasources/kafka_datasource.py
@@ -9,9 +9,12 @@ class KafkaDataSource(StreamDataSource, ABC):
     def __init__(self, connection: KafkaConnection):
         super().__init__(connection)
 
+        self._connection : KafkaConnection
+        self._default_timeout_ms = self._connection.DEFAULT_TIMEOUT_MS
+
     def get_broker_metadata(self) -> dict:
 
         metadata = self._connection_engine.list_topics(
-            timeout=self.DEFAULT_TIMEOUT_MS)
+            timeout=self._default_timeout_ms)
 
         return metadata
