cee8cd1d63f9b64ffebc13db56ae9d407b883789
red-robin-matand <red.robin.matand@gmail.com>
2025-10-02 21:18:51 +0000

    refactor: remove redundent abs method

--- src/data_access_layer/datasources/datalake_datasource.py
+++ src/data_access_layer/datasources/datalake_datasource.py
@@ -33,22 +33,3 @@ class DataLakeDataSource(DataSource, ABC):
     def append_to_table(self, namespace: str, table_name: str, data, partition_cols=None) -> None:
         pass
 
-    @abstractmethod
-    def overwrite_partitions(self, namespace: str, table_name: str, data, partition_filter: dict) -> None:
-        pass
-
-    @abstractmethod
-    def overwrite_table(self, namespace: str, table_name: str, data) -> None:
-        pass    
-
-    @abstractmethod
-    def evolve_table_schema(self, namespace: str, table_name: str, new_schema: dict) -> None:
-        pass    
-
-    @abstractmethod
-    def expire_snapshots(self, namespace: str, table_name: str, older_than_timestamp: int) -> None:
-        pass
-
-    @abstractmethod
-    def compact_table(self, namespace: str, table_name: str) -> None:
-        pass
